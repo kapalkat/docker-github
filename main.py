a9fd0815ed6daed2bca2bb85fb1395a18bcb8a8c
Tomasz Kapałka <tomasz.kapalka@amartus.com>
2018-03-23 14:53:45 +0100

    presentation_change

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True, host='0.0.0.0', port=8080)
-    # app.run(debug=True, threaded=True, port=8080)
+    # app.run(debug=True, threaded=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, threaded=True, port=8080)
 
